14271663da0511c2f48168e4e6e4421744c6c8c8
ppalladio <nine.slide@gmail.com>
2023-06-09 23:47:33 +0200

    stock image and general style

--- bank/src/App.jsx
+++ bank/src/App.jsx
@@ -1,7 +1,13 @@
+import styles from './style';
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>Navbar</div>
+        </div>
+		<div className={`${styles.flexStart} bg-primary`}>
 
-
-const App = () => {
-    return <div>App</div>;
-};
+		</div>
+    </div>
+);
 
 export default App;

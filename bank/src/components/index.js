8ea7746ef16380e626407f1012f658c25b420c92
ppalladio <nine.slide@gmail.com>
2023-06-11 22:28:05 +0200

    components added

---
+++ bank/src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from './Billing/Billing';
+import Business from './Business/Business';
+import CardDeal from './CardDeal/CardDeal';
+import CTA from './CTA/CTA';
+import Footer from './Footer/Footer';
+import Hero from './Hero/Hero';
+import Navbar from './Navbar/Navbar';
+import Stats from './Stats/Stats';
+import Testimonials from './Testimonials/Testimonials';
+import Clients from './Clients/Clients';
+
+export{
+	Navbar,
+	Billing,
+	CardDeal,
+	Business,
+	Clients,
+	CTA,
+	Footer,
+	Hero,
+	Stats,
+	Testimonials
+}
